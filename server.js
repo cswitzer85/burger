2920be045a12a000c12b5e5390e6dab4ed835427
Cody Switzer <cswitzer85@gmail.com>
2020-02-26 14:10:29 -0600

    created package.json and updated server.js to have mysql connection

--- server.js
+++ server.js
@@ -5,3 +5,29 @@ var app = express();
 
 // Application port to set by Heroku or set to 9001
 var PORT = process.env.PORT || 9001;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
