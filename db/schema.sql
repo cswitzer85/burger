bc9da8c780a846a02f6f13341867223567372d4e
Cody Switzer <cswitzer85@gmail.com>
2020-02-28 01:38:25 -0600

    updated database, css & button text

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burger_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(50) NOT NULL,
+	burger_name varchar(90) NOT NULL,
 	devoured boolean default false,
 	PRIMARY KEY (id)
 );
