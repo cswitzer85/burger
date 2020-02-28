6ffb34361fde5a9db3b6a290c3f3be3a21d56d2a
Cody Switzer <cswitzer85@gmail.com>
2020-02-27 22:04:31 -0600

    created burgers directory inside partials directory and changed order on server.js file

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 9001;
+
 var app = express();
 
 // Application port to set by Heroku or set to 9001
-var PORT = process.env.PORT || 9001;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+	extended: true
+}));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+	defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burger_controller.js");
@@ -20,7 +27,7 @@ var routes = require("./controllers/burger_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+	// Log (server-side) when our server has started
+	console.log("Server listening on: http://localhost:" + PORT);
 });
